630a1ab9b6c49ea4a96c3f126906e04a219211a2
PEDRO LUCAS MATOS DE LIMA <pedrolima0834@digitalcollege.internal>
2025-09-23 12:07:31 -0300

    estudo sobre if e else

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 
-const idade = 18;
+// const age = prompt('Digite sua idade:')
 
-if(idade > 18){
-    console.log("Você é maior de idade.")
-} else {
-    console.log("Você é menor de idade.")
-}
+// if(age >= 18){
+//     if( age >= 65){
+//         alert("Voce está na melhor idade.");
+//     } else {
+//         alert("Você é maior de idade.");
+//     }
+
+// } else {
+//     alert("Você é menor de idade.");
+// }
+
+// const age = prompt("Digite sua idade:");
+
+// if(age >= 65){
+//     alert("Você está na melhor idade.");
+// } else if( age >= 18){
+//     alert("Você é maior de idade");
+// } else if( age <= 18){
+//     alert("Você é menor de idade.");
+// }
+
+
+// const temperatura = prompt("Digite a temperatura");
+
+// if(temperatura >= 35){
+//     alert("calor extremo.");
+// } else if(temperatura >= 25){
+//     alert("clima quente.");
+// } else if(temperatura >= 15){
+//     alert("Clima agradável.");
+// } else if(temperatura >= 5){
+//     alert("Clima frio.");
+// } else {
+//     alert("frio intenso.")
+// }
+
+// const color = "red";
+
+// switch(color){
+//     case "green":
+//         alert("Go!");
+//         break;
+//     case "yellow":
+//         alert("Atention");
+//         break;
+//     case "red":
+//         alert("Stop!");
+//         break;
+//     default:
+//         alert("Invalid color!");
+// }
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+// };
+
+const palavra = prompt("Digite uma palavra:");
+for(let i = 0; i <= 4; i++)
+    console.log(palavra)
